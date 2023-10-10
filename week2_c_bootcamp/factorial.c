541dc209b108d51760d694c558ecf57b34bd4e7a
1alpakkor0 <146676554+1alpakkor0@users.noreply.github.com>
2023-10-10 14:27:39 +0000

    update

--- week2_c_bootcamp/factorial.c
+++ week2_c_bootcamp/factorial.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
-unsigned int factorial(unsigned int n){
-    if(n == 0);
-    return 1;
-    return n * factorial(n-1);
+int factorial(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    else{
+        return n * factorial(n - 1);
+    }
+
 }
+
+
 
 int main(){
     int user_input;
     printf("Please enter an integer: ");
     scanf("%d", &user_input);
 
-    printf("Factorial of %d is %d", user_input, factorial(user_input));
-    return 0;
+    if (user_input < 0){
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
+
+    else{
+         printf("Factorial of %d is %d", user_input, factorial(user_input));
+         return 0;
+        
+
+    }
+
 }
