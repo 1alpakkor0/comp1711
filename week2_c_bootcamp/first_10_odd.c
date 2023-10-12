89dd3c38dfc3ebc22e3294c0aff43a69cf71f0b6
1alpakkor0 <146676554+1alpakkor0@users.noreply.github.com>
2023-10-12 09:54:32 +0000

    update

---
+++ week2_c_bootcamp/first_10_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+
+    for (a = 1; a < 20; a++ )
+    {
+        if (a %2 != 0)
+        {
+             printf("%d is an odd number\n", a);
+        }
+           
+        
+
+    }
+    
+    return 0;
+    
+}
