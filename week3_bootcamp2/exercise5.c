3f50f44e18d9e8a4f1b5f65b378ab33da36461ac
1alpakkor0 <146676554+1alpakkor0@users.noreply.github.com>
2023-10-16 22:21:44 +0000

    update

---
+++ week3_bootcamp2/exercise5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+
+    printf("Please select an option between 1 to 5: ");
+    scanf("%d", &a);
+
+
+    if (a < 1 || a > 5)
+    {
+       printf("Error! Try again: ");
+    }
+
+    switch(a)
+    {
+        case 1:
+            printf("Option 1 has been selected\n");
+            break;
+
+        case 2:
+            printf("Option 2 has been selected\n");
+            break;
+        
+        case 3:
+            printf("Option 3 has been selected\n");
+            break;
+
+        case 4:
+            printf("Option 4 has been selected\n");
+            break;
+
+        case 5:
+            printf("Option 5 has been selected\n");
+            break;
+
+        default:
+            printf("A different option has been selected\n");
+    }
+}
