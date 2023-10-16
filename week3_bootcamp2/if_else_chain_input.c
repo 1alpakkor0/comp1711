b25c9edb1af5012bc65d4126b75b0219d4b8b493
1alpakkor0 <146676554+1alpakkor0@users.noreply.github.com>
2023-10-16 19:45:30 +0000

    update

---
+++ week3_bootcamp2/if_else_chain_input.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int mark;
+
+    printf ("Enter the mark: \n");
+    scanf ("%d", &mark);
+
+    if (mark == 0)
+    {
+        printf ("The mark of %d is zero\n", mark);
+    }
+    else if (mark >= 70 && mark <= 100)
+    {
+        printf ("The mark of %d is a first\n", mark);
+    }
+
+    else if (mark >= 60)
+    {
+        printf ("The mark of %d is a 2:1\n", mark);
+    }
+
+    else if (mark <= 40)
+    {
+        printf ("The mark of %d is a fail\n", mark);
+    }
+   
+    return 0;
+}
